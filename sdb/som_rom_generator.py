20f2390f01d52e836d2d2d551020c2a919484595
cospan <cospan@gmail.com>
2017-02-27 15:16:37 -0500

    Added the correct license to all files

--- sdb/som_rom_generator.py
+++ sdb/som_rom_generator.py
@@ -1,22 +1,19 @@
-#! /usr/bin/python
-
-# Copyright (c) 2015 Dave McCoy (dave.mccoy@cospandesign.com)
+# this file is part of SDB.
 #
-# This file is part of Nysa.
-# (http://wiki.cospandesign.com/index.php?title=Nysa.org)
-#
-# Nysa is free software; you can redistribute it and/or modify
+# SDB is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# any later version.
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Nysa is distributed in the hope that it will be useful,
+# SDB is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Nysa; If not, see <http://www.gnu.org/licenses/>.
+# along with SDB.  If not, see <http://www.gnu.org/licenses/>
+
+
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
